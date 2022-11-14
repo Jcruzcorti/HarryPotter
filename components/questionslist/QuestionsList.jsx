94912ca7e64752062c56293cfecdb95cbef2aa2f
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-11-14 10:47:58 -0300

    Logramos mapear y pintar el DOM en QuestionsList.jsx tanto con la información de la API y como la del mock/Questionshp.jsx para corresponder cada img con cada pregunta en particular

--- components/questionslist/QuestionsList.jsx
+++ components/questionslist/QuestionsList.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import styles from '../../styles/Home.module.css';
 import { Questionshp } from '../mock/Questionshp';
 
@@ -9,6 +9,16 @@ import { Questionshp } from '../mock/Questionshp';
 export default function QuestionsList({data}) {
 
 //  const {a} = useContext(Context)
+const [finishQuestions,setFinishQuestions] = useState(true);
+
+// const finishQuestions = []
+// useEffect(() => {
+  
+//   setFinishQuestions(finishQuestions)
+//   return () => {
+    
+//   }
+// }, [])
 
 
   return (
@@ -17,18 +27,39 @@ export default function QuestionsList({data}) {
             {data.map((item)=>(           
             <div key={item.id}>
               {
-                item.id===0
+                !finishQuestions
                 ?<h2>{item.apodo}</h2>
-                :<>
-                <h2>{item.personaje}</h2>
-                <Image alt={'Personajes de Harry Potter'} src={item.imagen} width={100} height={100} quality={100} className={styles.imgs} />  
-                <p className={styles.pFooter}>{item.casaDeHogwarts}</p>
-                </>
-          
+                :<div className={styles.div0}>              
+                  <div className={styles.div1}>
+                    <h2>{item.personaje}</h2>
+                    <Image alt={'Personajes de Harry Potter'} src={item.imagen} width={100} height={100} quality={100} className={styles.imgs} />  
+                  </div>
+
+                  <div className={styles.div2}>
+                    {Questionshp.map((q)=>(
+                      <div key={q.ids}>
+                          {
+                            item.id === q.ids
+                            ?<div className={styles.divQuestionsButtons}>
+                              <h3>{q.question}</h3>
+                              <h3>{q.options.map((op)=>(
+                                <button key={q.ids}>
+                                  {op.option}
+                                </button>
+                              ))}</h3>
+                            </div>
+                            :null
+                          }
+                      </div>
+                      )) 
+                    }
+                  </div>
+                </div>
               }
             </div>
-          ))}             
-         </div>  
+          ))}
+          
+        </div>  
   )
 }
 
